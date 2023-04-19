0778402418ae4d354fe7f02f1de4e0038ff781c3
Henrry Bolaños Rodriguez <i202015894@cibertec.edu.pe>
2023-04-19 02:10:44 -0500

    first commit

---
+++ Proyecto_EFSTRIII/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Proyecto_EFSTRIII.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto_EFSTRIII.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            IEnumerable<Inmueble> inmuebles = new InmuebleController().GetInmueble();
+            return View(inmuebles);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Proyecto_EFSTRIII/Models/Distrito.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto_EFSTRIII.Models
+{
+    public class Distrito
+    {
+        public int idDistrito { get; set; }
+        public string nombreDistrito { get; set; }
+    }
+}
